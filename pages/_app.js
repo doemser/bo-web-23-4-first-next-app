142500620f71a3bffcc74ea43060ac7a18f58b2d
Dominik Hautau <dominikhautau@gmail.com>
2023-08-29 10:46:48 +0200

    first next project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+function Layout({ children }) {
+  return (
+    <>
+      <header>Ich bin ein Header</header>
+      <hr />
+      {children}
+      <hr />
+      <footer>Ich bin ein Footer</footer>
+    </>
+  );
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
---
+++ pages/frogs/index.js
@@ -0,0 +1,3 @@
+export default function Frogs() {
+  return <>FROGS</>;
+}
